d226f9d04a797d2b67fadaebb9299e4647a1ec5d
mlctrez <mlctrez@gmail.com>
2016-07-15 13:18:59 -0500

    initial commit

---
+++ main.go
@@ -0,0 +1,27 @@
+package main
+
+import (
+	"github.com/gocraft/web"
+	"github.com/mlctrez/gowebsocket/chat"
+	"golang.org/x/net/websocket"
+	"net/http"
+)
+
+type Context struct {
+}
+
+func main() {
+	server := chat.NewServer()
+	go server.Listen()
+
+	router := web.New(Context{})
+
+	router.Middleware(web.StaticMiddleware("webroot"))
+
+	router.Get("/entry", func(rw web.ResponseWriter, req *web.Request) {
+		websocket.Handler(server.OnConnected).ServeHTTP(rw, req.Request)
+	})
+
+	http.ListenAndServe("localhost:8080", router)
+
+}
